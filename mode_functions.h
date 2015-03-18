ae0d9bfce1a8388c8cf740ffb00f8275c89adfe9
kuzinalx <kuzinalx@mail.ru>
2015-03-18 13:03:03 +0300

    commit without pull

--- mode_functions.h
+++ mode_functions.h
@@ -2,7 +2,7 @@
 #define MODE_FUNCTIONS_H
 
 #include <QTextStream>
-
+// comment
 void Integrate( QTextStream& in, QTextStream& out );
 void SolveODE( QTextStream& in, QTextStream& out );
 void SolveAlgEqv( QTextStream& in, QTextStream& out );
